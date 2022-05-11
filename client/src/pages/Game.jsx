0c3ec2c09f2f305350630fe93ce29f68cf227515
Chris <chris_lindstrom@outlook.com>
2022-05-11 10:46:30 +0200

    testing game

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -370,7 +370,7 @@ const Game = () => {
         const emptySquares = tempGameStateArray.filter(square => square === '0').length
         // Send game state to socket
         socket.emit('set-game-state', tempGameStateArray, placedPieces, currentPlayer === 'W' ? 'B' : 'W', blackPos, gameOver, wins)
-        if (emptySquares !== 0) {
+        if (emptySquares > 55) {
           // Change player after slight delay
           setTimeout(() => {
             setNewMsg(true);
@@ -378,7 +378,8 @@ const Game = () => {
             setCurrentPlayer(currentPlayer === 'W' ? 'B' : 'W')
           }, 1400);
         } else {
-          endGame(gameId, newScore)
+          // endGame(gameId, newScore)
+          endGame(gameId, { white: 32, black: 32 })
         }
       }
     }
